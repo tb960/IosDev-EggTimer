8cee1a1c4857e74d7bfb28d88ae08cf751a83e4d
BK Teo <khangtbk@gmail.com>
2021-05-08 15:38:46 +0800

    added a timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var conditionName: UILabel!
+    
+    var dict : [String : Int] = ["Soft":5, "Medium":6, "Hard":7]
+    var secondRemaining = 0
+    
     @IBAction func hardnessSelector(_ sender: UIButton) {
-        print(sender.currentTitle!)
+        //add exclamation mark here because we might not get a value from the sender title (
+        //see it's type by holding option and click on it
+        let hardness = sender.currentTitle!
+        //if we use dict[hardness] to access our dict value, we will get optional again, because the value that is return by the dict will be an optional because we will not know whether the dict will really have value
+        
+        //print(dict[hardness])
+        //print(dict[hardness]!)
+        switch hardness{
+        case "Soft":
+            secondRemaining = dict[hardness]!
+        case "Medium":
+            secondRemaining = dict[hardness]!
+        case "Hard":
+            secondRemaining = dict[hardness]!
+        default:
+            secondRemaining = 10
+        }
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
-
+    @objc func updateTimer(){
+        if secondRemaining != 0 {
+            print("\(secondRemaining) seconds.")
+            secondRemaining -= 1
+        }
+        else{
+            conditionName.text = "Done!"
+        }
+    }
+    
+    
 }
